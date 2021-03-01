250cfaa54e740553dc84e8d8fdc8f2001543d55d
matheus-paulino <matheuspaulino.santos1@gmail.com>
2021-02-28 23:45:46 -0300

    style list transactions

--- lib/main.dart
+++ lib/main.dart
@@ -55,7 +55,50 @@ class HomePage extends StatelessWidget {
           Column(
             children: _transcations.map((transaction) {
               return Card(
-                child: Text(transaction.title),
+                child: Row(
+                  children: <Widget>[
+                    Container(
+                      margin: EdgeInsets.symmetric(
+                        horizontal: 15.0,
+                        vertical: 10.0,
+                      ),
+                      decoration: BoxDecoration(
+                        border: Border.all(
+                          color: Colors.lightBlue,
+                          width: 2.0,
+                        ),
+                      ),
+                      padding: EdgeInsets.all(10.0),
+                      child: Text(
+                        transaction.value.toString(),
+                        style: TextStyle(
+                          fontWeight: FontWeight.bold,
+                          fontSize: 20.0,
+                          color: Colors.lightBlue,
+                        ),
+                      ),
+                    ),
+                    Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: <Widget>[
+                        Text(
+                          transaction.title,
+                          style: TextStyle(
+                            fontSize: 16,
+                            fontWeight: FontWeight.bold,
+                            color: Colors.lightBlue,
+                          ),
+                        ),
+                        Text(
+                          transaction.date.toString(),
+                          style: TextStyle(
+                            color: Colors.grey[600],
+                          ),
+                        ),
+                      ],
+                    )
+                  ],
+                ),
               );
             }).toList(),
           ),
